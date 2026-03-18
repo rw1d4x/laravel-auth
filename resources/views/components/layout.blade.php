5c159c8098546777b1bc8148ea22fc4a82a97779
Rida <rida.ahmidanelhassani@student.rocva.nl>
2026-03-18 09:21:36 +0100

    Week 7 Accessing the current user

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,15 +18,22 @@
       <h1>
         <a href="{{ route('ninjas.index') }}">Ninja Network</a>
       </h1>
-      <a href="{{ route('ninjas.create') }}">Create New Ninja</a>
-      
-      <a href="{{ route('show.login') }}" class="btn">Login</a>
-      <a href="{{ route('show.register') }}" class="btn">Register</a>
+      @guest
+        <a href="{{ route('show.login') }}" class="btn">Login</a>
+        <a href="{{ route('show.register') }}" class="btn">Register</a>
+      @endguest
 
-      <form action="{{ route('logout')}}" method="POST" class="m-0">
-        @csrf 
-        <button class="btn">Logout</button>
-      </form>
+      @auth
+        <span class="border-r-2 pr-2">
+          Hi there, {{Auth::user()->name}}
+        </span>
+        <a href="{{ route('ninjas.create') }}">Create New Ninja</a>
+
+        <form action="{{ route('logout')}}" method="POST" class="m-0">
+          @csrf 
+          <button class="btn">Logout</button>
+        </form>
+      @endauth
     </nav>
   </header>
 
